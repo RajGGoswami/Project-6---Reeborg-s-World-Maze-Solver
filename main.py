833a690ff4283d38ba50725f4a712f5c1135e016
RajGGoswami <109051791+RajGGoswami@users.noreply.github.com>
2026-01-08 20:03:45 +1300

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+# Day 6 – Reeborg's World Maze Solver
+# Part of my 100 Days of Code journey
+#
+# Learning goals for this project:
+# - Understanding problem-solving through algorithms
+# - Using while loops for repeated actions
+# - Applying conditional logic to navigate unknown paths
+# - Implementing the right-hand rule for maze solving
+# - Reading and responding to environment-based conditions
+#
+# This project was completed using Reeborg’s World,
+# an interactive environment designed to teach programming logic.
+#
+# Below are the maze worlds this solution works for:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=problem_world.json&url=user_world%3Aproblem_world.json
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=problem_world2.json&url=user_world%3Aproblem_world2.json
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=problem_world3.json&url=user_world%3Aproblem_world3.json
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while front_is_clear():
+#     move()
+# turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#
+#     else:
+#         turn_left()
